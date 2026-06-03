26dc8a585d258b34a9f33ddb3470bc922d4776f5
MuriloChinaglia <mchinaglia530@gmail.com>
2026-06-03 14:12:15 -0300

    fix: contornando erro de async_hooks do tanstack

--- vite.config.ts
+++ vite.config.ts
@@ -9,4 +9,14 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  define: {
+    // Isso impede que o código do TanStack quebre ao procurar variáveis globais de servidor
+    "process.env": {},
+  },
+  build: {
+    rollupOptions: {
+      // Força o Rollup a ignorar o erro do async_hooks e compilar o resto do código
+      external: ["node:async_hooks"],
+    },
+  },
 });
